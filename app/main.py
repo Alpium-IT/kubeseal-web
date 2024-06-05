d76938583304920f1887f9180d06552f38283af3
thikade <thomas.hikade@gmail.com>
2024-06-05 12:58:28 +0200

    omit/ignore namespace if scope cluster-wide (fixes issue #3)

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ async def main():
                 ui.notify(f"Value is required for secret {idx+1}!", type="negative")
                 return
             
-        if not namespace.value:
+        if not namespace.value and scope.value != SCOPE_CLUSTER_WIDE:
             ui.notify("Namespace is required!", type="negative")
             return
 
@@ -85,7 +85,7 @@ async def main():
             encryptedString = secret['encrypted']
             ks_command = secret['cmd']
             # append all encrypted strings to the output widget1/2
-            s1 = s1 + f"\n\n# cmd: echo -n '{secretVal}' | {ks_command} \n# kubesealed secret: value='{secretVal}'\n{encryptedString}"
+            s1 = s1 + f"\n\n# cmd: echo -n '{secretVal}' | {ks_command} \n# secret-key='{secretKey}'\n{encryptedString}"
             s2 = s2 + f"\n{encryptedString}"
             allSecrets = allSecrets + f"    {secretKey}: {encryptedString}\n"
 
@@ -94,7 +94,12 @@ async def main():
         widgets[2].set_text(s2.lstrip()) # set into hidden label for copying
         
         # also put into manifest widget
-        manifest = manifest_template.format(SCOPE=scope.value, SECRETNAME=secretName.value, NAMESPACE=ns, ENCRYPTEDSECRETS=(allSecrets.rstrip()))
+        if scope.value == SCOPE_CLUSTER_WIDE:
+            manifest_namespace = f"namespace: null"
+        else:
+            manifest_namespace = f"namespace: {ns}"
+
+        manifest = manifest_template.format(SCOPE=scope.value, SECRETNAME=secretName.value, NAMESPACE=manifest_namespace, ENCRYPTEDSECRETS=(allSecrets.rstrip()))
         widgets[5].set_value(manifest)
         widgets[6].set_text(manifest) # set into hidden label for copying
 
@@ -106,9 +111,15 @@ async def main():
         secretKey = secret['key'].strip()
         secretVal = secret['value'].strip()
 
-        ks_param_name = ""
-        if scope.value == 'strict':
+        if scope.value == SCOPE_STRICT:
             ks_param_name = f"--name {secretName.value}"
+        else:
+            ks_param_name = ""
+        
+        if scope.value == SCOPE_CLUSTER_WIDE:
+            ks_param_namespace = ""
+        else:
+            ks_param_namespace = f"--namespace '{ns}'"
 
         command1 = f"echo -n '{secretVal}'"
         process1 = await asyncio.create_subprocess_exec(
@@ -119,7 +130,7 @@ async def main():
         )
         os.close(write)
 
-        ks_command = f"kubeseal --cert {clusterConfig['clusters'][clusterName]['url']} --scope '{scope.value}' --namespace '{ns}' {ks_param_name} --raw --from-file=/dev/stdin"
+        ks_command = f"kubeseal --cert {clusterConfig['clusters'][clusterName]['url']} --scope '{scope.value}' {ks_param_namespace} {ks_param_name} --raw --from-file=/dev/stdin"
         process2 = await asyncio.create_subprocess_exec(
             *shlex.split(ks_command, posix='win' not in sys.platform.lower()),
             stdin=read,
@@ -233,7 +244,7 @@ async def main():
             ui.label("Secret NAMESPACE").classes("text-sky-600 font-bold w-24")
             namespace = ui.input(label='Namespace of the Kubernetes Secret', 
                         placeholder='e.g. demo', value="demo",
-                        validation={'Input too long: > 64 chars!': lambda value: len(value) < 64, 'Required': lambda value: len(value) > 0})
+                        validation={'Input too long: > 64 chars!': lambda value: len(value) < 64, 'Required': lambda value: len(value) > 0 or scope.value == SCOPE_CLUSTER_WIDE})
             namespace.props('size=80')
             
             # add PREFIX checkbox
@@ -330,7 +341,7 @@ apiVersion: bitnami.com/v1alpha1
 kind: SealedSecret
 metadata:
   name: {SECRETNAME}
-  namespace: {NAMESPACE}
+  {NAMESPACE}
   annotations:
     sealedsecrets.bitnami.com/{SCOPE}: "true"
 spec:
@@ -339,7 +350,7 @@ spec:
   template:
     metadata:
       name: {SECRETNAME}
-      namespace: {NAMESPACE} 
+      {NAMESPACE} 
       annotations:
         sealedsecrets.bitnami.com/{SCOPE}: "true"
 """.strip()
@@ -357,11 +368,16 @@ if os.path.isfile(configName):
 else:
     print(f"\nWARN: Config file '{configName}' not found! Using default config for demo purposes only.\n")
 
-SCOPES = ['strict', 'namespace-wide'] # , 'cluster-wide']
+
+SCOPE_STRICT = 'strict'
+SCOPE_NS_WIDE = 'namespace-wide'
+SCOPE_CLUSTER_WIDE = 'cluster-wide'
+
+SCOPES = [ SCOPE_STRICT, SCOPE_NS_WIDE ] 
 DEF_SCOPE_IDX = 1
 
 if clusterConfig['defaults'].get('enable-cluster-wide-encryption', False):
-    SCOPES.append('cluster-wide')
+    SCOPES.append(SCOPE_CLUSTER_WIDE)
 
 MAX_SECRETS = clusterConfig['defaults'].get('max-secrets', MAX_SECRETS)
 
